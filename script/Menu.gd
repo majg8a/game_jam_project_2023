b289183ff67a3313404666a2842aa79f5e538470
Edavi11 <82729630+Edavi11@users.noreply.github.com>
2023-02-05 10:41:19 -0400

    add walk animation n fix camera

--- script/Menu.gd
+++ script/Menu.gd
@@ -8,7 +8,9 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+				
 	$VBoxContainer/StartButton.grab_focus()
+	
 
 func _on_StartButton_pressed():
 	get_tree().change_scene("res://Scenes/world.tscn")
--- script/player.gd
+++ script/player.gd
@@ -3,16 +3,34 @@ extends KinematicBody2D
 var speed = 200
 var velocity = Vector2()
 
+onready var sprite = $Sprite
+onready var animationPlayer = $AnimationPlayer
+
 func _physics_process(delta):
 	velocity = Vector2()
 	
-	if Input.is_action_pressed("down"):
-		velocity.y += speed
-	if Input.is_action_pressed("up"):
-		velocity.y -= speed
-	if Input.is_action_pressed("left"):
-		velocity.x -= speed
-	if Input.is_action_pressed("right"):
-		velocity.x += speed
+	
+	if Input.is_action_pressed("down") or Input.is_action_pressed("up") or Input.is_action_pressed("left") or Input.is_action_pressed("right"):
+		if Input.is_action_pressed("down"):
+
+			velocity.y += speed
+			
+		elif Input.is_action_pressed("up"):
+
+			velocity.y -= speed
+
+		if Input.is_action_pressed("left"):
+			sprite.flip_h = true
+			velocity.x -= speed
+			
+		elif Input.is_action_pressed("right"):
+			sprite.flip_h = false
+			velocity.x += speed
+			
+		animationPlayer.play("Walk")	
 		
+	else: 
+		animationPlayer.play("idle")
+		
+	
 	move_and_slide(velocity)
