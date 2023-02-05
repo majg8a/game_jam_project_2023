044b0f4e58b116f5f6b9f8151fbc621ff21f07e5
edavi11 <erickdavila11@gmail.com>
2023-02-04 23:06:50 -0400

    adding menu to game

---
+++ script/Menu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/StartButton.grab_focus()
+
+func _on_StartButton_pressed():
+#	get_tree().change_scene()
+	pass
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+	
+
+
